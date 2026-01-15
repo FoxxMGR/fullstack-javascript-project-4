c139593cdb565783cc5317a52737d472fa45d5bc
FoxxMGR <magomacievgadzimurad@gmail.com>
2026-01-15 17:23:34 +0300

    вернулся к шагу 1.2

--- src/page-loger.js
+++ src/page-loger.js
@@ -16,15 +16,26 @@ import Listr from 'listr'
 const log = debug('page-loader')
 
 const pageLoader = (url, outputDir) => {
-
+const parseUrl = new URL(url)
+log('распарсенная ссылка', parseUrl)
+log('часть ссылки',`${parseUrl.host}${parseUrl.pathname}`)
+const urlWithoutProtocol = `${parseUrl.host}${parseUrl.pathname}`
+const filename = `${urlWithoutProtocol.replace(/[^a-zA-Z0-9]/g, '-')}.html`;
+log('название файла', filename)
   log('ссылка для скачивания', url)
   log('папка для скачивания', outputDir)
 axios.get(url)
-  .then(function (response) {
-    
-    log(' ' ,response.data)
+  .then(response => {
+    log(' ' , response.data)
+    return response.data
   })
-  
+  .then((data1) => {
+    log('ожидаем получить', data1)
+    fs.writeFile(`${outputDir}/${filename}`, data1)
+    return `${outputDir}/${filename}`
+})
+// .then(filename => console.log(filename))
 }
+
 
 export default pageLoader
